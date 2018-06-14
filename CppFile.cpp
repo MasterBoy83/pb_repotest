a936978cabfc5c2347285ebb6ecc5b32cc56fe81
Piotr Rybka <prybka@ra.rockwell.com>
2018-06-14 09:19:12 +0200

    Ver 1.0

---
+++ CppFile.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+
+void fun()
+{
+    //Fun
+}
+//Main
+int main()
+{
+    int tab[4] = {1,2,3,4};
+    int a = 9;
+    cout<<"Zmienna a:"<<a<<endl;
+
+    return 0;
+}
