ec8fbfdd48656c5af79f92f43e05b5774e69c8ae
Piotr <piotr.rybka87@gmail.com>
2018-06-14 11:32:59 +0200

    Update CppFile.cpp

--- CppFile.cpp
+++ CppFile.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+/// fun() ///
 
 void fun()
 {
@@ -14,6 +15,10 @@ int main()
     int tab[4] = {1,2,3,4};
     int a = 9;
     cout<<"Zmienna a:"<<a<<endl;
-
+    
+    for(int i =0;i<4;i++)
+    {
+        cout<<tab[i]<<endl;
+    }
     return 0;
 }
