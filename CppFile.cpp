8678bb296d021f38264033c022f6a95938b3e31c
Piotr <piotr.rybka87@gmail.com>
2018-06-28 16:54:01 +0200

    removed tabs

--- CppFile.cpp
+++ CppFile.cpp
@@ -22,12 +22,12 @@ int main()
     int tab[4] = {1,2,3,4};
     int a = 9;
     //call
-	//fun2()
-	cout<<"fun2()"<<endl;
+    //fun2()
+    cout<<"fun2()"<<endl;
     fun2();
     cout<<"Zmienna a:"<<a<<endl;
     int b = 44;
-	int c = a +b ;
+    int c = a + b;
     cout<<"Tab: "<<endl;
     for(int i = 0;i<4;i++)
     {
